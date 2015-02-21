03cb484c6b5cf18c6205995916aec89155206af3
Anders Fisher <atleastimtrying@gmail.com>
2015-02-21 18:16:22 +0000

    Auto-generated deployment commit

--- js/config.js
+++
@@ -1,6 +0,0 @@
-window.dice.config = {
-  stupid_tricks_enabled: false,
-  switch_tricks_enabled: false,
-  ramp_mode_enabled: false,
-  spins_enabled: false
-};
--- js/renderer.js
+++
@@ -1,7 +0,0 @@
-window.dice.Renderer = function(){
-  var render = function(event, message){
-    $('#display').html(message);
-    $('#exclude').data('exclude', message);
-  };
-  $(dice).on('render', render);
-};
