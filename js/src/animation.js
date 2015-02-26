d8a7b06b98598f2a9cf1a7aa65b8e5fcaea83459
Anders Fisher <atleastimtrying@gmail.com>
2015-02-26 09:54:19 +0000

    yay regexes!

--- js/src/animation.js
+++ js/src/animation.js
@@ -1,6 +1,9 @@
 window.dice.Animation = function(){
+  var amend_rotation_classes = function(tricks){
+    return tricks.replace(/\d/, 't$&');
+  };
   var animate = function(event, tricks){
-    tricks = tricks.replace('1', 't1').replace('2', 't2').replace('3', 't3');
+    tricks = amend_rotation_classes(tricks);
     $('.trick_display')[0].className = 'trick_display ' + tricks;
 
     setTimeout(function(){
